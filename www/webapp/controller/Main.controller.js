2676b61ec3e373a5247390cd80e245e685eac56b
radu103 <radu103@hotmail.com>
2019-12-03 10:21:08 +0200

    i18n for module loads ok

--- www/webapp/controller/Main.controller.js
+++ www/webapp/controller/Main.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"ro/dts/tm3/ui5/controller/BaseController",
 	"sap/ui/model/json/JSONModel",
+	"sap/ui/model/resource/ResourceModel",
 	"sap/ui/core/Fragment"
-], function(BaseController, JSONModel, Fragment) {
+], function(BaseController, JSONModel, ResourceModel, Fragment) {
 	"use strict";
 
 	return BaseController.extend("ro.dts.tm3.ui5.controller.Main", {
@@ -12,9 +13,6 @@ sap.ui.define([
 		onInit: function() {
 			console.log("Main Controller - onInit");
 
-			var oView = this.getView();
-			var oMainPage = oView.byId("mainPage");
-
 			this.oConfigModel = this.getCoreModel("configModel");
 			this.oModulesConfigModel = this.getCoreModel("modulesConfigModel");
 
@@ -22,31 +20,63 @@ sap.ui.define([
 
 			if(oModules && oModules.modules){
 
+
 				// core modules add
-				for(var m=0; m < oModules.modules.core.length; m++){
-					
-					var launchFragment = Fragment.load({ 
-						"name" : "ro.dts.tm3.ui5.modules.core." + oModules.modules.core[m] + ".Launch" 
-					}).then(function(launchFragment){
-						oMainPage.addContent(launchFragment);
-					});
-					
-					var newModule = {
-						"name": oModules.modules.core[m],
-						"routes" : [],
-						"i18n" : null
-					};
-
-					// TO DO : load custom routes to oRouter
-
-					// TO DO : load specific i18n resource bundles
-
-					this.oModulesConfigModel.setProperty("/" + oModules.modules.core[m], newModule);
-				}
+				this.loadCoreModules(oModules.modules);
 
 				// plugin modules add
-				// TO DO
+				this.loadPluginModules(oModules.plugins);
 			}
+		},
+
+		loadCoreModules: function(modules){
+			
+			var oView = this.getView();
+
+			for(var m = 0; m < modules.core.length; m++){
+					
+				var oModule = modules.core[m];
+				
+				var newModuleConfig = {
+					"name": oModule.name,
+					"routes" : [],
+					"i18n" : null
+				};
+
+				// load custom routes to oRouter
+				var oModuleRoutes = new JSONModel();
+				oModuleRoutes.loadData("modules/core/" + oModule.name + "/routes.json", null, false);
+				var routes = oModuleRoutes.getData();
+				newModuleConfig.routes = routes;
+				console.log(routes);
+				
+				// TO DO : create routes in main router
+
+				// load specific i18n resource bundles
+				var oModuleTexts = new ResourceModel({
+					"bundleName" : "ro.dts.tm3.ui5.modules.core.clients.i18n.i18n",
+					"defaultBindingMode" : "OneWay",
+					"async" : false
+				});
+
+				newModuleConfig.i18n = oModuleTexts.getResourceBundle();
+				this.setModel(oModuleTexts, oModule.name + "_i18n");
+
+				// save module configuration
+				this.oModulesConfigModel.setProperty("/" + oModule.name, newModuleConfig);
+
+				// create launch fragment
+				var launchFragment = Fragment.load({ 
+					"name" : "ro.dts.tm3.ui5.modules.core." + oModule.name + ".Launch" 
+				}).then(function(launchFragment){
+					var oControl = oView.byId(oModule.controlId);
+					oControl.addContent(launchFragment);
+				});
+			}
+		},
+
+		loadPluginModules: function(plugins){
+			// TO DOs
 		},
 
 		onShellSearch : function(){
